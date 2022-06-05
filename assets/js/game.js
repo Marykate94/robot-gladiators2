a573858db08af86f87d100efe59e33190bc046a9
Marykate Smith <Mkayy@Marykates-MacBook-Pro.local>
2022-06-05 10:24:00 -0400

    added variable to store playername input

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,8 @@
-window.alert("This is an alert! JavaScript is running!");
+// this creates a function named "fight"
+var playerName = window.prompt("What is your robots name?");
+
+function fight() {
+    window.alert("The fight has begun!");
+}
+
+fight();
